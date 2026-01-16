3e74d6a8de43bfdafaf48bc1afa49e7de6eddbf2
Jennifer Reyes <Jennireyes1010@gmail.com>
2026-01-15 20:36:28 -0800

    Add capitalize() utility function

--- journal.js
+++ journal.js
@@ -10,3 +10,7 @@ function doubleNumber(number) {
 function squareNumber(number) {
     return number * number;
 }
+function capitalize(text) {
+    if (text.length === 0) return text;
+    return text[0].toUpperCase() + text.slice(1)
+}
